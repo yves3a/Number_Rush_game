74ce4455ac8841292e5993e78cc533cfe7819fea
yves3a <yves.irakoz3@gmail.com>
2025-07-09 15:08:03 +0200

    Initialize the game when the DOM is fully loaded

---
+++ src/utils/shaka.js
@@ -0,0 +1,10 @@
+import { Game } from './game.js';
+// Initialize the game when the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', () => {
+  // Create and initialize the game
+  const game = new Game();
+  game.init();
+  
+  // Expose the game to the global scope for debugging purposes
+  window.shakaGame = game;
+});
